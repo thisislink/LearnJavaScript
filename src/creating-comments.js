71303abc8fff5135828fa30c346f514d85ef4e8b
Daphne Link <daphnelink@gmail.com>
2020-10-04 16:54:25 -0700

    Creating comments
    learn how to create comments in JS

---
+++ src/creating-comments.js
@@ -0,0 +1,20 @@
+// I'm a one line comment 
+
+/* I'm a multi-line comment
+
+
+Notice how I can't be on 
+
+multiple
+lines and 
+
+I don't cause any errors.
+
+*/
+
+/* To start a multi-line comment, use the forward-slash and the asterisk symbol (*) with no space. 
+Then type your comment or your notes after those characters. 
+To end the multi-line comment, use the asterisk symbol (*) and the back-slash with no space.
+
+Remember, anything you type between the starting and ending characters will be a comment */
+
