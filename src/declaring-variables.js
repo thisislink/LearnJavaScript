b9bd45ae05f3f44bc0a68f59dafe74a00b1b68c9
Daphne Link <daphnelink@gmail.com>
2020-10-05 19:29:19 -0700

    declaring variables

---
+++ src/declaring-variables.js
@@ -0,0 +1,28 @@
+let creditCard = 'Visa'; // this assigns the value Visa to a variable called creditCard
+let cardBalance = '10.00'; //this assigns the value of 10.00 to a variable called cardBalance
+const creditCardLimit = '1000.00'; //this assigns the value 1000.00 to a variable called creditCardLimit and it cannot be changed because of the const variable declaration
+
+/*
+You can create aka declare variable names as anything you want.
+
+For example, if you wanted to create a variable to hold kitchen items you can do something like
+
+let kitchenItems = 'fork';
+
+Try to make sure your variable names are descriptive, instead of using x, a, etc.
+
+There are a few other rules to variables you must follow:
+
+They must start with a letter, an underscore, or a dollar sign.
+They also must not be a reserved JavaScript keyword, such as let, import, int, string, etc.
+
+You will learn more JavaScript keywords as you progress.
+*/
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+/* Note: it's worth mentioning, you might come across older JavaScript files that use the var keyword to declare variables. 
+
+Best practice is to only use let and const. However, if you see var it is used the same way. 
+
+The issue with var is it can cause issues in regards to scope. You will learn more about scope later on.
+
+*/
